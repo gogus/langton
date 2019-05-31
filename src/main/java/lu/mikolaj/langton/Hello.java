50f507395704b1b285cda716d225aa6fa8dcc3a9
Mikolaj Gogula <mikolaj.gogula@gmail.com>
2019-05-31 21:18:05 +0200

    Application init.

---
+++ src/main/java/lu/mikolaj/langton/Hello.java
@@ -0,0 +1,13 @@
+package lu.mikolaj.langton;
+
+public class Hello {
+    private String text;
+
+    public Hello(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
